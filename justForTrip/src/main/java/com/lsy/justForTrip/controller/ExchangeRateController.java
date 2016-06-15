ff0230d32c518ab8cca30c412d3cd6a62ebdd977
Seyoung <Seyoung@192.168.200.200>
2016-06-16 00:01:18 +0900

    설정파일 변경

--- justForTrip/src/main/java/com/lsy/justForTrip/controller/ExchangeRateController.java
+++ justForTrip/src/main/java/com/lsy/justForTrip/controller/ExchangeRateController.java
@@ -3,16 +3,17 @@ package com.lsy.justForTrip.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lsy.justForTrip.domain.CommonDomain;
 import com.lsy.justForTrip.domain.ExchangeRate;
 import com.lsy.justForTrip.service.ExchangeRateService;
 
-@Controller
+@RestController
 @RequestMapping("/exchangeRates")
 public class ExchangeRateController {
 
@@ -25,7 +26,7 @@ public class ExchangeRateController {
 	
 	@RequestMapping(value="", method=RequestMethod.GET)
 	@ResponseBody
-	public List<ExchangeRate> getExchangeList (CommonDomain domain) {
+	public List<ExchangeRate> getExchangeList (@RequestParam(required=false) CommonDomain domain) {
 		return exchangeRateService.getExchangeRates(domain);
 	}
 }
